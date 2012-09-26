9a190e80de390797b86ed4aab97b694add0a2fdb
turtlebot <turtlebot@demobot.(none)>
2012-09-25 20:55:57 -0400

    Transferred 'Emo-Faces' to IHeartRobotics Android repo.

---
+++ src/com/org/iheartengineering/emofaces/Emo.java
@@ -0,0 +1,61 @@
+package com.org.iheartengineering.emofaces;
+
+/**
+ * An object representation representation of an emoticon
+ * @author Errol Markland
+ */
+public class Emo {
+	Emo() {
+	}
+	
+	public Emo(String name, String emoticon) {
+		this.Name = name;
+		this.Emoticon = emoticon;
+		this.Source = null;
+	}
+	
+	public Emo(String name, String emoticon, String source) {
+		this.Name = name;
+		this.Emoticon = emoticon;
+		this.Source = source;
+	}
+	
+	/**
+	 *  The name of the emotion
+	 */
+	public String Name;
+	
+	/**
+	 * The text representation of the emotion
+	 */
+	public String Emoticon;
+	
+	/**
+	 * The path to the image file (optional).
+	 */
+	public String Source;
+	
+	@Override
+	public int hashCode() {
+		int hash = 13;
+		hash = 31 * hash + Name.hashCode();
+		hash = 31 * hash + Emoticon.hashCode();
+		hash = 31 * hash + ((Source == null) ? 0 : Source.hashCode());
+		return hash;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (obj.getClass() != this.getClass())  return false;
+		
+		Emo that = (Emo)obj;
+		if ((this.Name != that.Name) || (this.Emoticon != that.Emoticon)) return false;
+		
+		//TODO: May remove this?
+		if ((this.Source != null) && (this.Source != that.Source)) return false;
+		
+		return true;
+	}
+}
