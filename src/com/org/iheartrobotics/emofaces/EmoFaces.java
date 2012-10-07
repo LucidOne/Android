2a1075bcffe2f5f2d2bda4c7a93f5fd1b93fa700
Errol Markland <ja23jr@gmail.com>
2012-10-07 19:12:08 -0400

    Removed main activity. EmoFacesAndroid now holds the main running structure.

--- src/com/org/iheartrobotics/emofaces/EmoFaces.java
+++ src/com/org/iheartrobotics/emofaces/EmoFaces.java
@@ -25,18 +25,19 @@ public class EmoFaces {
 	 * Adds a list of emotions to the current set of emotions
 	 * @param emotions - The list of emotions to be added
 	 */
-	public void addEmotions(ArrayList<Emo> emotions) {
+	public void addEmoList(ArrayList<Emo> emotions) {
 		for (Emo e : emotions) {			
 			addEmotion(e);
 		}
 	}
 	
-	public void addEmotionsFromFile(InputStream inputStream) {
+	public void addEmoList(InputStream inputStream) {
 		EmoFaceReader reader = new EmoFaceReader();
 		for (Emo e : reader.readEmoticonsFromStream(new InputStreamReader(inputStream))) {
 			emotions.put(e.name,  e);
 		}
 	}
+	
 	/**
 	 * Get emotions from file
 	 * @param filename - File which contains the emotions to be added
@@ -58,7 +59,7 @@ public class EmoFaces {
 	 * @param name - The name of the emoticon
 	 * @return The emo object, which contains the emoticon and optional source file, or null if the emo doesn't exist
 	 */
-	public Emo getEmotion(String name) {
+	public Emo getEmo(String name) {
 		return emotions.containsKey(name) ? emotions.get(name) : null;
 	}
 	
@@ -68,5 +69,20 @@ public class EmoFaces {
 	 */
 	public Set<String> getAvailableEmotions() {
 		return emotions.keySet();
+	}
+	
+	/**
+	 * Updates an existing emo
+	 * @param name - The name of the emo
+	 * @param emoticon - The emoticon representing the emo
+	 * @param source - The file representing the emo
+	 */
+	public void setEmo(String name, String emoticon, String source) {
+		
+	}
+	
+	public void setEmo(String name, String emoticon) 
+	{
+		setEmo(name, emoticon, null);
 	}
 }
--- src/com/org/iheartrobotics/emofaces/EmoFacesAndroid.java
+++ src/com/org/iheartrobotics/emofaces/EmoFacesAndroid.java
@@ -2,10 +2,12 @@ package com.org.iheartrobotics.emofaces;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.res.Resources;
+import android.os.Bundle;
 import android.os.Handler;
 import android.util.TypedValue;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
@@ -14,46 +16,101 @@ import android.widget.RelativeLayout.LayoutParams;
 
 import java.io.*;
 import java.util.Random;
+import java.util.Set;
 
 /**
  * Android implementation of the Emo-Faces Program
  * @author Errol Markland
  * TODO: Improve this....overall structure...it's rather messy =x
  */
-public class EmoFacesAndroid {
+public class EmoFacesAndroid extends Activity {
 	public static int fontSize = 128;
 	EmoFaces emotions = null;
 	TextView textView = null;
-	Activity activity = null;
-	RelativeLayout layout = null;
 	public boolean randomFaces = false;
 	public int interval = 1500;
-	public Handler handle = null;
-	Random random = new Random();
 	Runnable r = new Runnable() {
 		public void run() {
 			update();
-			handle.postDelayed(this, interval);
+			handler.postDelayed(this, interval);
 		}
 	};
 	
-	public EmoFacesAndroid(Handler handle) 
-	{
-		this.handle = handle; 
+	private RelativeLayout relativeLayout = null;
+	private Handler handler = null;
+	static Random random = new Random();
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+	    Init();
+    }
+	
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+        
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+    	int scale = 32;
+        switch (item.getItemId()) {
+        	case R.id.quit:
+        		Stop();
+        		return true;
+        	case R.id.bigger:
+        		fontSize += scale;
+        		return true;
+        	case R.id.smaller:
+        		fontSize -= scale;
+        		if (fontSize < 0) {
+        			fontSize = scale;
+        		}
+        		return true;        		
+        }
+        return false;
+    }
+    
+    class TouchListener implements OnTouchListener {
+		public boolean onTouch(View v, MotionEvent event) {
+			if (event.getAction() == MotionEvent.ACTION_DOWN) {
+				String nextEmo = getNextEmotion(emotions.emotions.keySet(), emotions.currentEmotion.name);
+				if (nextEmo == null) {
+					throw new IllegalArgumentException("Failed to find argument.");
+				}
+				emotions.currentEmotion = emotions.emotions.get(nextEmo);
+				textView.setText(emotions.currentEmotion.Emoticon);
+				setContentView(relativeLayout);
+				return true;
+			}
+			return false;
+		}
+    }
+    
+    private String getNextEmotion(Set<String> keySet, String currentEmotion) {
+		String[] keys = keySet.toArray(new String[0]);
+		int i = -1;
+		while (i < 0) {
+			i = random.nextInt(keys.length) - 1;
+		}
+		return keys[i];
 	}
 	
-	public void Init(Activity activity, RelativeLayout layout)
+	public void Init()
 	{
-		this.activity = activity;
-		this.layout = layout;
 		if (emotions == null) {
+	        relativeLayout = new RelativeLayout(this);
+	        handler = new Handler();
 			emotions = new EmoFaces();
 			// Load default emotions
 			LoadDefaultEmotions();
 			emotions.currentEmotion = emotions.emotions.get("default");
 			textView = GetCurrentEmotion();
-			layout.addView(textView, setLayoutParams());
-			activity.setContentView(layout);
+			relativeLayout.addView(textView, setLayoutParams());
+			setContentView(relativeLayout);
 			
 			setListeners();
 		}
@@ -61,14 +118,14 @@ public class EmoFacesAndroid {
 	
 	void setListeners() {
 		// random face guy
-		handle.postDelayed(new Runnable() {
+		handler.postDelayed(new Runnable() {
 			public void run() {
 				update();
-				handle.postDelayed(this, interval);
+				handler.postDelayed(this, interval);
 			}
 		}, interval);
 		
-		layout.setOnTouchListener(new OnTouchListener() {
+		relativeLayout.setOnTouchListener(new OnTouchListener() {
 			public boolean onTouch(View v, MotionEvent event) {
 				if (event.getAction() == MotionEvent.ACTION_DOWN) {
 					update();
@@ -92,7 +149,7 @@ public class EmoFacesAndroid {
 		emotions.currentEmotion = emotions.emotions.get(nextEmo);
 		textView.setText(emotions.currentEmotion.Emoticon);
 		textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, fontSize);
-		activity.setContentView(layout);
+		setContentView(relativeLayout);
 	}
 	
 	private RelativeLayout.LayoutParams setLayoutParams() 
@@ -112,8 +169,8 @@ public class EmoFacesAndroid {
 	
 	private void LoadDefaultEmotions() {
 		try {
-			InputStream s = this.activity.getAssets().open("face_packs/default.json");
-			emotions.addEmotionsFromFile(s);
+			InputStream s = getAssets().open("face_packs/default.json");
+			emotions.addEmoList(s);
 		} catch (IOException e) {
 			System.out.println("Failed to load face packs");
 			e.printStackTrace();
@@ -122,7 +179,7 @@ public class EmoFacesAndroid {
 		
 	@TargetApi(11)
 	public TextView GetCurrentEmotion() {
-		TextView view = new TextView(this.activity);
+		TextView view = new TextView(this);
         String e = emotions.currentEmotion.Emoticon;
         if (e == null) {
         	e = "N/A";
@@ -138,7 +195,7 @@ public class EmoFacesAndroid {
 	 * Stops the application.
 	 */
 	public void Stop() {
-		handle.removeCallbacks(r);
-		this.activity.finish();
+		handler.removeCallbacks(r);
+		finish();
 	}
 }
